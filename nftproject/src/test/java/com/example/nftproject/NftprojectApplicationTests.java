da00cb3ce9dc3f3514578fc4a899ed9b7753c3b4
zwh996 <1317839481@qq.com>
2023-01-19 11:23:04 +0800

    wrote a readme file

---
+++ nftproject/src/test/java/com/example/nftproject/NftprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nftproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NftprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
