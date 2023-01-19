a77b9f199c9d26c8cfa032e8f90d33966b9fa8e8
zwh996 <1317839481@qq.com>
2023-01-19 16:52:28 +0800

    new file:   java/.gitignore 	new file:   java/.mvn/wrapper/maven-wrapper.jar 	new file:   java/.mvn/wrapper/maven-wrapper.properties 	new file:   java/mvnw 	new file:   java/mvnw.cmd 	new file:   java/pom.xml 	new file:   java/src/main/java/com/example/backend/NftprojectApplication.java 	new file:   java/src/main/resources/application.properties 	new file:   java/src/test/java/com/example/backend/NftprojectApplicationTests.java

---
+++ java/src/main/java/com/example/backend/NftprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NftprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NftprojectApplication.class, args);
+    }
+
+}
